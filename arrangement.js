999e346fce67fec1f75e5a6d8f96abe677178388
ajrhind <alanah.rhind009@gmail.com>
2023-05-03 17:30:57 +1200

    fheyed

--- arrangement.js
+++ arrangement.js
@@ -10,7 +10,7 @@ let lastSwapTime = 0;
 const millisPerSwap = 3000;
 
 // global variables for colors
-const bg_color1 = [100];
+const bg_color1 = [0];
 function setup () {
   // create the drawing canvas, save the canvas element
   let main_canvas = createCanvas(canvasWidth, canvasHeight);
@@ -41,16 +41,21 @@ function draw () {
   // reset the random number generator each time draw is called
   randomSeed(curRandomSeed);
 
-  // clear screen
+  
+  
   background(bg_color1);
+
+  for(i=0; i<20; i++){
+    noStroke();
+    fill(180,20);
+    strokeWeight(10);
+    triangle(480,-150,180*i/12,500,780*i/15,500);
+  } 
+
+  // clear screen
   noStroke();
 
-  // for(let i=0; i=5;i++){
-  //   stroke(0);
-  //   strokeWeight(3);
 
-  //   ellipse(i*2,i*1);
-  // }
   // draw a 7x4 grid of faces
   let w = canvasWidth / 4;
   let h = canvasHeight / 2;
@@ -68,15 +73,7 @@ function draw () {
         let sideRight = random(-1,1);
 
 
-        // let is_cyclops = random(0, 100);
-
-        // if(is_cyclops < 10) {
-        //   eye_value = 1;
-        //   tilt_value = random(-5, 5);
-        //   mouth_value = random(0, 1.7);
-        // }
-
-        push();
+      push();
         let place = map(sideRight,-1,1,10,90);
         let upsie = map(eyes,1.8,3,130,150);
         translate(place*10, upsie*2);
@@ -86,8 +83,9 @@ function draw () {
         scale(w/25, h/25);
         
         myFace(cheek,nose,eyes,temples,mouth,sideRight)
-        pop();
-      
+          // fill(0,i);  
+      pop();
+            
     }
   }
 }
